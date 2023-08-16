284e778396a19213a5bed91d4f9ada3e723e493c
Tayná Carvalho <taynacarvalhogomes@gmail.com>
2023-08-16 19:35:21 -0400

    add

--- SalvandoEsteProjeto-main/NovoTayUmDoce/Janelas/CadastrarFuncionario.xaml.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Janelas/CadastrarFuncionario.xaml.cs
@@ -63,6 +63,9 @@ namespace NovoTayUmDoce.Janelas
             }
         }
 
-       
+        private void tbSalario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
