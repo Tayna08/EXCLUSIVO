ba7be23a95baf167ed345ea082d8455875b0bb44
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-08-16 19:16:17 -0400

    atualização

--- SalvandoEsteProjeto-main/NovoTayUmDoce/Models/Funcionario.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Models/Funcionario.cs
@@ -9,22 +9,19 @@ namespace NovoTayUmDoce.Models
     class Funcionario
     {
         public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime? Data { get; set; }
+        public string Cpf { get; set; }
+        public string Contato { get; set; }
+        public string Funcao { get; set; }
+        public string Email { get; set;}
+        public string Salario { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
-        public string Funcao { get; set; }
-        public string Complemento { get; set; }
-        public string Cpf { get; set; }
-        public string Salario { get; set; }
-        public string Rg { get; set; }
-        public string Nome { get; set; }
-        public string Cep { get; set; }
         public string Rua { get; set; }
+        public string Complemento { get; set; }
         public int Numero { get; set; }
-        public DateTime? Data { get; set; }
-        public string Contato { get; set; }
-
-
-
+        
     }
 
 }
