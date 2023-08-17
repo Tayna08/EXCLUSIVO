451f297ffd09696684aaf928aa33982abc5cd1e4
Tayná Carvalho <taynacarvalhogomes@gmail.com>
2023-08-16 20:41:27 -0400

    att

--- SalvandoEsteProjeto-main/NovoTayUmDoce/Janelas/CadastrarFuncionario.xaml.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Janelas/CadastrarFuncionario.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using NovoTayUmDoce.Models;
+using TayUmDoceProjeto.Models;
 
 namespace NovoTayUmDoce.Janelas
 {
