f64417ea1b723809c1d51f68a9c990f1f557519c
Tayná Carvalho <taynacarvalhogomes@gmail.com>
2023-08-21 14:49:24 -0400

    att conexão e att cliente

--- SalvandoEsteProjeto-main/NovoTayUmDoce/Models/Cliente.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Models/Cliente.cs
@@ -12,11 +12,6 @@ namespace TayUmDoceProjeto.Models
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public string Contato { get; set; }
-        public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Complemento { get; set; }
         public DateTime? DataNasc { get; set; }
-        public string Rua { get; set; }
-        public int Numero { get; set; }
     }
 }
