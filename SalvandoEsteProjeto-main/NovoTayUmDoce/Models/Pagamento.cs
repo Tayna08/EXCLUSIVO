f5373155974e2b0f8e46c5c1a624e154fe6cf7ac
rangelmarchiori <amandarangelmarchiori@gmail.com>
2023-09-25 23:45:22 -0300

    pagamento
    verificar a FK

---
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Models/Pagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NovoTayUmDoce.Models;
+
+namespace NovoTayUmDoce.Models
+{
+    internal class Pagamento
+    {
+        public int Id { get; set; }
+        public double Valor { get; set; }
+        public DateTime? DataPag { get; set; }
+
+    }
+}
