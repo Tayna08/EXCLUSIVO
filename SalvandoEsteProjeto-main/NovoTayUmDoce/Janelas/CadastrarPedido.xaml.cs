a6d0cc1b086c72bd221f5cc333602f65f1168d9d
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-10-15 10:02:14 -0400

    atualizacao na tela pedido

--- SalvandoEsteProjeto-main/NovoTayUmDoce/Janelas/CadastrarPedido.xaml.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Janelas/CadastrarPedido.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TayUmDoceProjeto.Janelas;
 
 namespace NovoTayUmDoce.Janelas
 {
@@ -24,14 +25,44 @@ namespace NovoTayUmDoce.Janelas
             InitializeComponent();
         }
 
-        private void btSalvar_Click(object sender, RoutedEventArgs e)
+    
+
+        private void btProduto_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarProduto produto = new CadastrarProduto();
+            produto.ShowDialog();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
         private void btCancelar_Click(object sender, RoutedEventArgs e)
         {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente cancelar o cadastro?", "Pergunta", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
+            if (result == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPagamento_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarPagamento pagamento= new CadastrarPagamento();
+            pagamento.ShowDialog();
+        }
+
+        private void btCliente_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarCliente cliente= new CadastrarCliente();
+            cliente.ShowDialog();
         }
     }
 }
