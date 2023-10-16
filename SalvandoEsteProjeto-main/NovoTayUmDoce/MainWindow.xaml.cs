855164163d9aed6a898570862e5c3969032778e2
rangelmarchiori <amandarangelmarchiori@gmail.com>
2023-10-16 11:42:19 -0300

    Home
    Faltando Ligação

--- SalvandoEsteProjeto-main/NovoTayUmDoce/MainWindow.xaml.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TayUmDoceProjeto.Janelas;
 using NovoTayUmDoce.Janelas;
+using MaterialDesignThemes.Wpf;
+using NovoTayUmDoce.Models;
 
 namespace NovoTayUmDoce
 {
@@ -25,9 +27,54 @@ namespace NovoTayUmDoce
         public MainWindow()
         {
             InitializeComponent();
+
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("Cliente"));
+            menuRegister.Add(new SubItem("Funcionario"));
+            menuRegister.Add(new SubItem("Employees"));
+            menuRegister.Add(new SubItem("Products"));
+            var item6 = new ItemMenu("Registro", menuRegister, PackIconKind.Register);
+
+            var menuSchedule = new List<SubItem>();
+            menuSchedule.Add(new SubItem("Abrir/Fechar Caixa" ));
+            menuSchedule.Add(new SubItem("Historico do Caixa"));
+            var item1 = new ItemMenu("Caixa", menuSchedule, PackIconKind.BoxAdd);
+
+            var menuReports = new List<SubItem>();
+            menuReports.Add(new SubItem("Customers"));
+            menuReports.Add(new SubItem("Providers"));
+            menuReports.Add(new SubItem("Products"));
+            menuReports.Add(new SubItem("Stock"));
+            menuReports.Add(new SubItem("Sales"));
+            var item2 = new ItemMenu("Reports", menuReports, PackIconKind.FileReport);
+
+            var menuExpenses = new List<SubItem>();
+            menuExpenses.Add(new SubItem("Fixed"));
+            menuExpenses.Add(new SubItem("Variable"));
+            var item3 = new ItemMenu("Expenses", menuExpenses, PackIconKind.ShoppingBasket);
+
+            var menuFinancial = new List<SubItem>();
+            menuFinancial.Add(new SubItem("Cash flow"));
+            var item4 = new ItemMenu("Financial", menuFinancial, PackIconKind.ScaleBalance);
+
+            var item0 = new ItemMenu("Dashboard", new UserControl(), PackIconKind.ViewDashboard);
+
+            Menu.Children.Add(new ControleHome(item0));
+            Menu.Children.Add(new ControleHome(item6));
+            Menu.Children.Add(new ControleHome(item1));
+            Menu.Children.Add(new ControleHome(item2));
+            Menu.Children.Add(new ControleHome(item3));
+            Menu.Children.Add(new ControleHome(item4));
+
         }
 
-        //Cadastrar e Listar Ciente
+        private void menuSchedule (object sender, RoutedEventArgs e))
+        {
+        
+            CadastrarCliente cliente = new CadastrarCliente();
+             cliente.ShowDialog();
+        }
+        /*Cadastrar e Listar Ciente
         private void btCadastrarCliente_Click(object sender, RoutedEventArgs e)
         {
             CadastrarCliente cliente = new CadastrarCliente();
@@ -192,6 +239,7 @@ namespace NovoTayUmDoce
         {
             ListarRecebimento listarRecebimento = new ListarRecebimento();
             listarRecebimento.ShowDialog();
-        }
+        }*/
+
     }
 }
