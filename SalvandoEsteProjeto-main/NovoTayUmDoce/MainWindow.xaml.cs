01d05e0195b715f69e7931e2d10e28812ce5db6c
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-10-18 16:09:38 -0400

    att

--- SalvandoEsteProjeto-main/NovoTayUmDoce/MainWindow.xaml.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/MainWindow.xaml.cs
@@ -61,12 +61,12 @@ namespace NovoTayUmDoce
             var item1 = new ItemMenu("Caixa", menuSchedule, PackIconKind.BoxAdd);
 
             var menuReports = new List<SubItem>();
-            menuReports.Add(new SubItem("Customers"));
+            menuReports.Add(new SubItem("Despesa"));
             menuReports.Add(new SubItem("Providers"));
             menuReports.Add(new SubItem("Products"));
             menuReports.Add(new SubItem("Stock"));
             menuReports.Add(new SubItem("Sales"));
-            var item2 = new ItemMenu("Reports", menuReports, PackIconKind.FileReport);
+            var item2 = new ItemMenu("Financeiro", menuReports, PackIconKind.FileReport);
 
             var menuExpenses = new List<SubItem>();
             menuExpenses.Add(new SubItem("Fixed"));
