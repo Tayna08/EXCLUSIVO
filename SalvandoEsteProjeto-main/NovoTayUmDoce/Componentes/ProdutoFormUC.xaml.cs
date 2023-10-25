b46a03d1c143fb8d8f24355c61bcc3e570d4e033
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-10-24 22:00:09 -0400

    DESISTO DE PRODUTO DA PROXIM OLHAAAAAAAAAAAAAAAAAA O BD CARAMBAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- SalvandoEsteProjeto-main/NovoTayUmDoce/Componentes/ProdutoFormUC.xaml.cs
+++ SalvandoEsteProjeto-main/NovoTayUmDoce/Componentes/ProdutoFormUC.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NovoTayUmDoce.Conexão;
+using NovoTayUmDoce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,19 +22,71 @@ namespace NovoTayUmDoce.Componentes
     /// </summary>
     public partial class ProdutoFormUC : UserControl
     {
-        public ProdutoFormUC()
+        MainWindow _context;
+        private static Conexao conn;
+
+        public ProdutoFormUC(MainWindow context)
         {
             InitializeComponent();
+            _context = context;
+            CarregarData();
+        }
+
+        private void CarregarData()
+        {
+            try
+            {
+                cbPed.ItemsSource = null;
+                cbPed.Items.Clear();
+                cbPed.ItemsSource = new FuncionarioDAO().List();
+                cbPed.DisplayMemberPath = "Nome";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Não Executado", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void btCancelar_Click(object sender, RoutedEventArgs e)
         {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente cancelar o Pedido?", "Pergunta", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
+            if (result == MessageBoxResult.Yes)
+            {
+                _context.SwitchScreen(new PedidoFormUC(_context));
+            }
         }
 
         private void btSalvar_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Produto produto = new Produto();
 
+                produto.Nome= tbNOme.Text;
+                //produto.Peso = tbP
+
+
+                // Chaves estrangeiras
+                //pedido.Cliente = (Cliente)cbCli.SelectedItem;
+                // pedido.Funcionario = (Funcionario)cbFun.SelectedItem;
+
+                // Inserindo os Dados           
+                
+                MessageBox.Show("Dados salvos com sucesso!");
+                Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar os dados: " + ex.Message);
+            }
+
+
+        }
+        private void Clear()
+        {
+           
         }
 
         private void cbPed_SelectionChanged(object sender, SelectionChangedEventArgs e)
